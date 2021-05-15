582a85b210f349b8c2ea0ad6f91bfe872bfaa99b
alexis <alexis.garant@gmail.com>
2021-05-15 17:06:52 -0400

    working with external c code

--- lib/screen/TakePictureScreen.dart
+++ lib/screen/TakePictureScreen.dart
@@ -1,7 +1,19 @@
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:ffi/ffi.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as imglib;
+
+import 'ImagePreview.dart';
+
+typedef convert_func = Pointer<Uint32> Function(
+    Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, Int32, Int32, Int32, Int32);
+typedef Convert = Pointer<Uint32> Function(
+    Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, int, int, int, int);
 
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
@@ -19,6 +31,13 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
   CameraController _controller;
   Future<void> _initializeControllerFuture;
 
+  CameraImage _savedImage;
+
+  final DynamicLibrary convertImageLib = Platform.isAndroid
+      ? DynamicLibrary.open("libconvertImage.so")
+      : DynamicLibrary.process();
+  Convert conv;
+
   @override
   void initState() {
     super.initState();
@@ -30,6 +49,11 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
       // Define the resolution to use.
       ResolutionPreset.high,
     );
+
+    // Load the convertImage() function from the library
+    conv = convertImageLib
+        .lookup<NativeFunction<convert_func>>('convertImage')
+        .asFunction<Convert>();
 
     // Next, initialize the controller. This returns a Future.
     _initializeControllerFuture = _controller.initialize().then((_) {
@@ -48,7 +72,9 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
   }
 
   void _processCameraImageStream(CameraImage image) {
-    //print(image.format);
+    setState(() {
+      _savedImage = image;
+    });
   }
 
   @override
@@ -63,8 +89,7 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             // If the Future is complete, display the preview.
-            print(MediaQuery.of(context)
-                .orientation); // todo link phone orientation with camera orientation
+            //print(MediaQuery.of(context).orientation); // todo link phone orientation with camera orientation
             return Stack(
               fit: StackFit.expand,
               children: [
@@ -85,12 +110,51 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
           // Take the Picture in a try / catch block. If anything goes wrong,
           // catch the error.
           try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
+            Pointer<Uint8> p = calloc(_savedImage.planes[0].bytes.length);
+            Pointer<Uint8> p1 = calloc(_savedImage.planes[1].bytes.length);
+            Pointer<Uint8> p2 = calloc(_savedImage.planes[2].bytes.length);
 
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
+            // Assign the planes data to the pointers of the image
+            Uint8List pointerList =
+                p.asTypedList(_savedImage.planes[0].bytes.length);
+            Uint8List pointerList1 =
+                p1.asTypedList(_savedImage.planes[1].bytes.length);
+            Uint8List pointerList2 =
+                p2.asTypedList(_savedImage.planes[2].bytes.length);
+            pointerList.setRange(0, _savedImage.planes[0].bytes.length,
+                _savedImage.planes[0].bytes);
+            pointerList1.setRange(0, _savedImage.planes[1].bytes.length,
+                _savedImage.planes[1].bytes);
+            pointerList2.setRange(0, _savedImage.planes[2].bytes.length,
+                _savedImage.planes[2].bytes);
+
+            // Call the convertImage function and convert the YUV to RGB
+            Pointer<Uint32> imgP = conv(
+                p,
+                p1,
+                p2,
+                _savedImage.planes[1].bytesPerRow,
+                _savedImage.planes[1].bytesPerPixel,
+                _savedImage.planes[0].bytesPerRow,
+                _savedImage.height);
+            List imgData =
+                imgP.asTypedList((_savedImage.width * _savedImage.height));
+
+            // Generate image from the converted data
+            imglib.Image img = imglib.Image.fromBytes(
+                _savedImage.height, _savedImage.width, imgData);
+
+            // Free the memory space allocated
+            // from the planes and the converted data
+            calloc.free(p);
+            calloc.free(p1);
+            calloc.free(p2);
+            calloc.free(imgP);
+
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ImagePreview(img: img)));
           } catch (e) {
             // If an error occurs, log the error to the console.
             print(e);
